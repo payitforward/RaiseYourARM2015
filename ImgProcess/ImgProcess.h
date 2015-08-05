f2e31af1a20ef37b067886011ad5ebb3f1573cec
Hoang Trung Hieu <hthhieu@gmail.com>
2015-08-05 16:08:55 +0700

    Release 0.0.2

--- ImgProcess/ImgProcess.h
+++
@@ -1,7 +0,0 @@
-#ifndef __IMGPROCESS_H_
-#define __IMGPROCESS_H_
-
-void imageProcessInit();
-void imageProcessTask();
-void imgProcessRecv(uint8_t c);
-#endif
--- Manual/manual.h
+++
@@ -1,7 +0,0 @@
-#ifndef __MANUAL_H_
-#define __MANUAL_H_
-
-void manualTask();
-void manualRecv(uint8_t c);
-#endif
-
